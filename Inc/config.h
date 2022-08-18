f5d547615443b9be5e823c4bd3f623f0a9323111
tzoy91 <78653123+tzoy91@users.noreply.github.com>
2022-08-18 22:29:17 +0300

    sinus 1200 25 phase adv

--- Inc/config.h
+++ Inc/config.h
@@ -159,7 +159,7 @@
 // Field Weakening / Phase Advance
 #define FIELD_WEAK_ENA  1               // [-] Field Weakening / Phase Advance enable flag: 0 = Disabled (default), 1 = Enabled
 #define FIELD_WEAK_MAX  5               // [A] Maximum Field Weakening D axis current (only for FOC). Higher current results in higher maximum speed. Up to 10A has been tested using 10" wheels.
-#define PHASE_ADV_MAX   45              // [deg] Maximum Phase Advance angle (only for SIN). Higher angle results in higher maximum speed.
+#define PHASE_ADV_MAX   25              // [deg] Maximum Phase Advance angle (only for SIN). Higher angle results in higher maximum speed.
 #define FIELD_WEAK_HI   1000            // (1000, 1500] Input target High threshold for reaching maximum Field Weakening / Phase Advance. Do NOT set this higher than 1500.
 #define FIELD_WEAK_LO   750             // ( 500, 1000] Input target Low threshold for starting Field Weakening / Phase Advance. Do NOT set this higher than 1000.
 
